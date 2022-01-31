ec0982f32dcb37e09934411ef218763c92ab7443
LuckyForce <adrian.schauer@aon.at>
2022-01-31 11:49:04 +0100

    chnaged how it checks what the user already selected before

--- src/consent-manager-v1.js
+++ src/consent-manager-v1.js
@@ -5,8 +5,38 @@
 //Version: 1.0.0
 //Description: This JavaScript file helps managing Cookie Selection in a completely custom way.
 
-const defaultCookie = window.localStorage.getItem("defaultCookie");
-const essentialCookie = window.localStorage.getItem("essentialCookie");
-const performanceCookie = window.localStorage.getItem("performanceCookie");
-const analyticsCookie = window.localStorage.getItem("analyticsCookie");
-const cookiesAcceptedOn = window.localStorage.getItem("cookiesAcceptedOn");
+if(window.localStorage.getItem("defaultCookieAllowed") == true){
+    const defaultCookieAllowed = true;
+}else{
+    const defaultCookieAllowed = false;
+}
+if(window.localStorage.getItem("essentialCookieAllowed") == true){
+    const essentialCookieAllowed = true;
+}else{
+    const essentialCookieAllowed = false;
+}
+if(window.localStorage.getItem("analyticsCookieAllowed") == true){
+    const analyticsCookieAllowed = true;
+}else{
+    const analyticsCookieAllowed = false;
+}
+if(window.localStorage.getItem("advertisingCookieAllowed") == true){
+    const advertisingCookieAllowed = true;
+}else{
+    const advertisingCookieAllowed = false;
+}
+if(window.localStorage.getItem("cookiesAcceptedOn") !== null){
+    const cookiesAcceptedOn = new Date(window.localStorage.getItem("cookiesAcceptedOn"));
+    if(cookiesAcceptedOn.getTime() < new Date(settings.cookiesChangedOn).getTime()){
+        const showConsent = true;
+        //Defines that consent was already given at some point int the past.
+        const consentChanged = true;
+    }
+}else{
+    const showConsent = true;
+}
+
+//Only Show Consent if Consent was given before change of settings
+if(showConsent){
+
+}
